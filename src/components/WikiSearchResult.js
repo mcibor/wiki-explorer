323c5743e5eef709d3c8f3d73b7db77eab17a347
mc <mc@mc.net>
2021-03-14 17:31:35 +0100

    rename to wiki-explorer

--- src/components/WikiSearchResult.js
+++ src/components/WikiSearchResult.js
@@ -4,7 +4,7 @@ class WikiSearchResult extends Component{
     render(){
         const {articles, links} = this.props.searchResult
         const result = articles.map((article, index) => {
-          return <li key={index}><a href={links[index]}>{article}</a></li>
+          return <li key={index}><a href={links[index]} target="_blank" rel="noreferrer">{article}</a></li>
         })
 
         return (
